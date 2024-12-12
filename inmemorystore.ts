51f85fa10e3ffef508d46b64b77d9b5165e54ca3
Sugam Banskota <sugam.banskota@yco.com.np>
2024-12-12 10:50:55 +0545

    WIP: message summarization

--- inmemorystore.ts
+++ inmemorystore.ts
@@ -2,7 +2,12 @@ import "dotenv/config";
 
 import { v4 as uuidv4 } from "uuid";
 import { ChatOpenAI } from "@langchain/openai";
-import { HumanMessage } from "@langchain/core/messages";
+import {
+  SystemMessage,
+  HumanMessage,
+  AIMessage,
+  RemoveMessage,
+} from "@langchain/core/messages";
 import {
   Annotation,
   StateGraph,
@@ -11,6 +16,7 @@ import {
   LangGraphRunnableConfig,
   messagesStateReducer,
   InMemoryStore,
+  END,
 } from "@langchain/langgraph";
 import readline from "readline";
 
@@ -21,17 +27,21 @@ const StateAnnotation = Annotation.Root({
     reducer: messagesStateReducer,
     default: () => [],
   }),
+  summary: Annotation<string>({
+    reducer: (_, action) => action,
+    default: () => "",
+  }),
 });
 
 const model = new ChatOpenAI({
   model: "gpt-4o-mini",
   temperature: 0,
 });
-
+//todo: update the store with the summary after summarization is done
 const callModel = async (
   state: typeof StateAnnotation.State,
   config: LangGraphRunnableConfig
-): Promise<{ messages: any }> => {
+): Promise<{ messages: any; summary: string }> => {
   const store = config.store;
   if (!store) {
     throw new Error("store is required when compiling the graph");
@@ -51,12 +61,74 @@ const callModel = async (
     { type: "system", content: systemMsg },
     ...state.messages,
   ]);
-  return { messages: response };
+  const summary = state.summary;
+  return { messages: [response], summary };
 };
 
+const shouldContinue = (
+  state: typeof StateAnnotation.State
+): "summarize_conversation" | typeof END => {
+  const messages = state.messages;
+  // If there are more than six messages, then we summarize the conversation
+  if (messages.length > 6) {
+    return "summarize_conversation";
+  }
+  // Otherwise we can just end
+  return END;
+};
+
+async function summarizeConversation(
+  state: typeof StateAnnotation.State
+): Promise<{ messages: any[]; summary: string }> {
+  // First, we summarize the conversation
+  const { summary, messages } = state;
+  let summaryMessage: string;
+  if (summary) {
+    // If a summary already exists, we use a different system prompt
+    // to summarize it than if one didn't
+    summaryMessage =
+      `This is summary of the conversation to date: ${summary}\n\n` +
+      "Extend the summary by taking into account the new messages above:";
+  } else {
+    summaryMessage = "Create a summary of the conversation above:";
+  }
+
+  const allMessages = [
+    ...messages,
+    new HumanMessage({
+      id: uuidv4(),
+      content: summaryMessage,
+    }),
+  ];
+  const response = await model.invoke(allMessages);
+  // We now need to delete messages that we no longer want to show up
+  // I will delete all but the last two messages, but you can change this
+  const deleteMessages = messages
+    .slice(0, -2)
+    .map((m) => new RemoveMessage({ id: m.id ?? "" }));
+  if (typeof response.content !== "string") {
+    throw new Error("Expected a string response from the model");
+  }
+  return { summary: response.content, messages: deleteMessages };
+}
+
 const builder = new StateGraph(StateAnnotation)
+  // Define the conversation node and the summarize node
   .addNode("call_model", callModel)
-  .addEdge(START, "call_model");
+  .addNode("summarize_conversation", summarizeConversation)
+  // Set the entrypoint as conversation
+  .addEdge(START, "call_model")
+  // We now add a conditional edge
+  .addConditionalEdges(
+    // First, we define the start node. We use `conversation`.
+    // This means these are the edges taken after the `conversation` node is called.
+    "call_model",
+    // Next, we pass in the function that will determine which node is called next.
+    shouldContinue
+  )
+  // We now add a normal edge from `summarize_conversation` to END.
+  // This means that after `summarize_conversation` is called, we end.
+  .addEdge("summarize_conversation", END);
 
 const graph = builder.compile({
   checkpointer: new MemorySaver(),
@@ -69,7 +141,7 @@ const rl = readline.createInterface({
 });
 
 let conversationCount = 0;
-const maxConversations = 3;
+const maxConversations = 6;
 let config = { configurable: { thread_id: "1", userId: "1" } };
 
 rl.question("Enter your initial query: ", async (initialInput) => {
